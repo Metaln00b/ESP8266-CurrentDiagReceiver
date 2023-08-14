88cbfa7d7e296a509aa4ca6860a40c7a220ed66a
Florian <Metaln00b@web.de>
2023-08-14 21:03:01 +0200

    ESP8266: Adjust speed calculation and disable causing problems

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include <AsyncElegantOTA.h>
 #include <WiFiUdp.h>
 #include <ESP8266WiFiGratuitous.h>
-#include <TFT_eSPI.h>       // Hardware-specific library
+#include <TFT_eSPI.h>
 #include <SPI.h>
 
 #define DRAW_DIGITS
@@ -18,9 +18,8 @@
   #define TTF_FONT NotoSans_Bold
 #endif
 
-TFT_eSPI display = TFT_eSPI();  // Invoke custom library
-
-TFT_eSprite spr = TFT_eSprite(&display);  // Declare Sprite object "spr" with pointer to "tft" object
+TFT_eSPI display = TFT_eSPI();
+TFT_eSprite spr = TFT_eSprite(&display);
 
 #ifdef DRAW_DIGITS
 OpenFontRender ofr;
@@ -34,17 +33,18 @@ AsyncWebServer server(80);
 #define SCREEN_WIDTH 240
 #define SCREEN_HEIGHT 280
 #define SCREEN_OFFSET 50
-//#define TFT_BL D2
 
 #define SPEED_SENSOR_PIN D2
 #define K_FACTOR 4127.0
 
-volatile unsigned int pulseCount = 0;
-unsigned long lastPulseTime = 0;
-float speed_kmh = 0;
+// volatile unsigned int pulseCount = 0;
+// unsigned long lastPulseTime = 0;
+unsigned long lastUdpProcessTime = 0;
+unsigned long udpProcessInterval = 100;
+// float speed_kmh = 0;
 
-#define AUDI_RED 0x4800 //RGB565
-#define AUDI_HIGHLIGHTED_RED 0xF980 //RGB565
+#define AUDI_RED 0x4800 // RGB565
+#define AUDI_HIGHLIGHTED_RED 0xF980 // RGB565
 
 const int EXDURATION = 2;
 const int TIMEOUT = 2;
@@ -72,7 +72,7 @@ void showClients()
   number_client = wifi_softap_get_station_num();
   stat_info = wifi_softap_get_station_info();
   
-  Serial.print("Connected clients: ");
+  Serial.print(F("Connected clients: "));
   Serial.println(number_client);
 
   while (stat_info != NULL)
@@ -81,9 +81,9 @@ void showClients()
       address = IPaddress->addr;
 
       Serial.print(cnt);
-      Serial.print(": IP: ");
+      Serial.print(F(": IP: "));
       Serial.print((address));
-      Serial.print(" MAC: ");
+      Serial.print(F(" MAC: "));
 
       uint8_t *p = stat_info->bssid;
       Serial.printf("%02X:%02X:%02X:%02X:%02X:%02X", p[0], p[1], p[2], p[3], p[4], p[5]);
@@ -110,38 +110,38 @@ void eventCb(System_Event_t *evt)
 
 void initWiFi()
 {
-  Serial.println("Setting soft-AP ... ");
+  Serial.println(F("Setting soft-AP ... "));
   wifi_set_event_handler_cb(eventCb);
 
   IPAddress apIP(192, 168, 4, 1);
 
   if (WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0)))
-    Serial.println("softAPConfig: True");
+    Serial.println(F("softAPConfig: True"));
   else
-    Serial.println("softAPConfig: False");
+    Serial.println(F("softAPConfig: False"));
 
   if (WiFi.softAP(ssid, pass, 8))
-    Serial.println("softAP: True");
+    Serial.println(F("softAP: True"));
   else
-    Serial.println("softAP: False");
+    Serial.println(F("softAP: False"));
   
   IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
+  Serial.print(F("AP IP address: "));
   Serial.println(IP);
 
   if (WiFi.status() != WL_CONNECTED){
     WiFi.disconnect();
-    WiFi.begin("CurrentDiag", pass);
+    WiFi.begin(F("CurrentDiag"), pass);
     Serial.println();
-    Serial.print("Wait for WiFi");
+    Serial.print(F("Wait for WiFi"));
 
     while (WiFi.status() != WL_CONNECTED) {
       delay(500);
-      Serial.print(".");
+      Serial.print(F("."));
     }
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: " + WiFi.localIP().toString());
+    Serial.println();
+    Serial.println(F("WiFi connected"));
+    Serial.println(F("IP address: ") + WiFi.localIP().toString());
   }
 
   AsyncElegantOTA.begin(&server);
@@ -150,15 +150,6 @@ void initWiFi()
 
   Udp.begin(PORT);
   experimental::ESP8266WiFiGratuitous::stationKeepAliveSetIntervalMs(5000);
-}
-
-void printDisplay(const char *string, int duration_s) {
-  display.fillScreen(AUDI_RED);
-  display.setTextSize(3);
-  display.setTextColor(AUDI_HIGHLIGHTED_RED);
-  display.setCursor(0, 0 + SCREEN_OFFSET);
-  display.println(string);
-  delay(duration_s * 1000);
 }
 
 void drawValueBar(int x, int y, int width, int height, float valueMin, float valueMax, float value, float vlineValue, bool showNumbers) {
@@ -174,24 +165,89 @@ void drawValueBar(int x, int y, int width, int height, float valueMin, float val
   display.fillRect(x + start, y + 2, end - start, height - 4, AUDI_HIGHLIGHTED_RED);
 
   if (height >= 15 && showNumbers) {
+    char roundValueStr[10];
+    dtostrf(roundValue, 5, 2, roundValueStr);
+    
     if (value >= vlineValue) {
       display.setTextSize(3);
       display.setTextColor(AUDI_HIGHLIGHTED_RED);
       display.setCursor(int(width / 9), y + 5);
-      display.println(String(roundValue, 2));
+      display.println(roundValueStr);
     } else {
       display.setTextSize(3);
       display.setTextColor(AUDI_HIGHLIGHTED_RED);
       display.setCursor(int(width / 1.5), y + 5);
-      display.println(String(roundValue, 2));
+      display.println(roundValueStr);
     }
   }
 
   display.drawFastVLine(pxPositionVline, y - 1, height + 2, AUDI_HIGHLIGHTED_RED);
 }
 
-void IRAM_ATTR pulseCounter() {
+/* void IRAM_ATTR pulseCounter() {
   pulseCount++;
+} */
+
+void processUdpPackets() {
+  int packetSize = Udp.parsePacket();
+
+  if (packetSize) {
+    char packetBuffer[255];
+    int len = Udp.read(packetBuffer, 255);
+
+    if (len > 0) {
+      packetBuffer[len] = '\0';
+      const char* msg = packetBuffer;
+
+      display.setTextSize(3);
+      display.setTextColor(AUDI_RED);
+
+      display.fillRect(100, 0 + SCREEN_OFFSET, 280 - 100, 22, AUDI_RED);
+      display.fillRect(0 + 1, 25 + SCREEN_OFFSET + 1, 280 - 2, 22 - 2, AUDI_RED);
+
+      display.fillRect(120, 50 + SCREEN_OFFSET, 280 - 120, 22, AUDI_RED);
+      display.fillRect(0 + 1, 75 + SCREEN_OFFSET + 1, 280 - 2, 22 - 2, AUDI_RED);
+
+      display.setTextColor(AUDI_HIGHLIGHTED_RED);
+
+      // JSON-Daten parsen und anzeigen
+      DynamicJsonDocument data(255);
+      DeserializationError error = deserializeJson(data, msg);
+
+      if (error) {
+        display.fillScreen(AUDI_RED);
+        display.println(F("Error parsing JSON!"));
+      } else {
+        float sensor1Value = atof(data["sensor1"]);
+        float sensor2Value = atof(data["sensor2"]);
+
+        char sensor1ValueStr[10];
+        char sensor2ValueStr[10];
+        dtostrf(sensor1Value, 6, 2, sensor1ValueStr);
+        dtostrf(sensor2Value, 3, 2, sensor2ValueStr);
+
+        display.setCursor(0, 0 + SCREEN_OFFSET);
+        display.print(F("Act.: "));
+        display.print(sensor1ValueStr);
+        display.println(F("mA"));
+        drawValueBar(0, 25 + SCREEN_OFFSET, 280, 22, v1Min, v1Max, sensor1Value, v1Vline, false);
+
+        display.setCursor(0, 50 + SCREEN_OFFSET);
+        display.print(F("Lambda: "));
+        display.print(sensor2ValueStr);
+        display.println();
+        drawValueBar(0, 75 + SCREEN_OFFSET, 280, 22, v2Min, v2Max, sensor2Value, v2Vline, false);
+      }
+
+/*       display.fillRect(0, 125 + SCREEN_OFFSET, 280, 22, AUDI_RED);
+      display.setCursor(0, 125 + SCREEN_OFFSET);
+
+      char speed_kmhStr[10];
+      dtostrf(speed_kmh, 5, 0, speed_kmhStr);
+      display.print(speed_kmhStr);
+      display.println(F(" km/h")); */
+    }
+  }
 }
 
 void setup() {
@@ -201,91 +257,41 @@ void setup() {
     ; // Needed for native USB port only
   }
 
-  //analogWrite(TFT_BL, 255);
-  
   display.init();
   display.setRotation(1);
   display.setTextColor(AUDI_HIGHLIGHTED_RED);
   display.fillScreen(AUDI_RED);
   display.setTextSize(3);
   display.setCursor(0, 0 + SCREEN_OFFSET);
-  display.println("Connecting...");
+  display.println(F("Connecting..."));
 
   initWiFi();
   delay(1000);
   display.fillScreen(AUDI_RED);
+
   pinMode(SPEED_SENSOR_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(SPEED_SENSOR_PIN), pulseCounter, RISING);
+//  attachInterrupt(digitalPinToInterrupt(SPEED_SENSOR_PIN), pulseCounter, RISING);
 }
 
 void loop() {
-  // Empfangen und Verarbeiten der Daten
-  //display.fillScreen(AUDI_RED);
-
   unsigned long currentTime = millis();
-  if (currentTime - lastPulseTime >= 1000) {
+  
+/*   if (currentTime - lastPulseTime >= 1000) {
+    
     float timeElapsed = (float)(currentTime - lastPulseTime) / 1000.0;  // Zeit in Sekunden
     float distance_km = (float)pulseCount / (float)K_FACTOR; // Entfernung in Kilometern
     speed_kmh = (distance_km / timeElapsed * 3600.0) / 10.0;
     
     lastPulseTime = currentTime;
+    detachInterrupt(digitalPinToInterrupt(SPEED_SENSOR_PIN));
     pulseCount = 0;
+    attachInterrupt(digitalPinToInterrupt(SPEED_SENSOR_PIN), pulseCounter, RISING);
+  } */
+
+  if (currentTime - lastUdpProcessTime >= udpProcessInterval) {
+    processUdpPackets();
+    lastUdpProcessTime = currentTime;
   }
 
-  
-  while (true) {
-    int packetSize = Udp.parsePacket();
-
-    if (packetSize) {
-      char packetBuffer[255];
-      int len = Udp.read(packetBuffer, 255);
-
-      if (len > 0) {
-        packetBuffer[len] = 0;
-        String msg = String(packetBuffer);
-
-        // Daten verarbeiten und auf dem Display anzeigen
-        //display.fillScreen(AUDI_RED);
-        display.setTextSize(3);
-        display.setTextColor(AUDI_RED);
-        //display.setCursor(0, 0 + SCREEN_OFFSET);
-
-        display.fillRect(100, 0 + SCREEN_OFFSET, 280 - 100, 22, AUDI_RED);
-        display.fillRect(0 + 1, 25 + SCREEN_OFFSET + 1, 280 - 2, 22 - 2, AUDI_RED);
-
-        display.fillRect(120, 50 + SCREEN_OFFSET, 280 - 120, 22, AUDI_RED);
-        display.fillRect(0 + 1, 75 + SCREEN_OFFSET + 1, 280 - 2, 22 - 2, AUDI_RED);
-
-        display.setTextColor(AUDI_HIGHLIGHTED_RED);
-
-        // JSON-Daten parsen und anzeigen
-        DynamicJsonDocument data(255);
-        DeserializationError error = deserializeJson(data, msg);
-
-        if (error) {
-          display.fillScreen(AUDI_RED);
-          display.println("Error parsing JSON!");
-        } else {
-          String actuatorString = "Act.: " + String(data["sensor1"].as<const char*>()) + "mA";
-          display.setCursor(0, 0 + SCREEN_OFFSET);
-          display.println(actuatorString);
-          drawValueBar(0, 25 + SCREEN_OFFSET, 280, 22, v1Min, v1Max, float(data["sensor1"]), v1Vline, false);
-
-          String lambdaString = "Lambda: " + String(data["sensor2"].as<const char*>());
-          display.setCursor(0, 50 + SCREEN_OFFSET);
-          display.println(lambdaString);
-          drawValueBar(0, 75 + SCREEN_OFFSET, 280, 22, v2Min, v2Max, float(data["sensor2"]), v2Vline, false);
-        }
-
-        display.fillRect(0, 125 + SCREEN_OFFSET, 280, 22, AUDI_RED);
-        display.setCursor(0, 125 + SCREEN_OFFSET);
-        display.print(speed_kmh, 0);
-        display.println(F(" km/h"));
-
-        break;
-      }
-    }
-  }
-
-  delay(150);
+  yield();
 }
